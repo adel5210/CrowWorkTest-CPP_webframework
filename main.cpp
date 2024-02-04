d6b6d50868b199002fa2b30e7d564e9c8c4b0b2d
adel <adel_5210m@yahoo.com>
2024-02-04 10:33:10 +0800

    With html page

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "crow.h"
+#include "iostream"
 
 int main() {
+
+    std::cout << std::system("pwd")<<std::endl;
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")([](){
-       return "Hellow CROW";
+       return "Hello CROW";
     });
+
+    CROW_ROUTE(app,"/test_page")([](){
+        auto page = crow::mustache::load_text("test.html");
+        return page;
+    });
+
     app.port(18080).multithreaded().run();
 }
